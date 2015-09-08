fd5c9487aaf34cd1c6fbddcb6ba448bc1277425d
Phil Cooper <phillycooper@hotmail.com>
2015-09-08 16:47:13 +0100

    Remove list link

--- app/components/List.js
+++ app/components/List.js
@@ -1,12 +1,16 @@
 var React = require('react');
 var listStore = require('../stores/listStore');
 var listActions = require('../actions/listActions');
+var Router = require('react-router');
+
+var Link = Router.Link;
 
 var List = React.createClass({
     getInitialState: function () {
         return listStore.getState();
     },
     componentDidMount: function () {
+        listActions.getData();
         listStore.on('change', this.handleChange);
     },
     componentWillUnmount: function () {
@@ -15,19 +19,16 @@ var List = React.createClass({
     handleChange: function () {
         this.setState(listStore.getState());
     },
-    handleClick: function () {
-        listActions.getData();
-    },
     render: function () {
         var listItems = this.state.items.map(function (item) {
             return <li>{item.src}</li>;
         });
         return (
             <div>
-                <button type="button" onClick={this.handleClick}>Next page</button>
                 <ul>
                     {listItems}
                 </ul>
+                <Link to="/">Home</Link>
             </div>
         )
     }
